a7a477957d2052f0c7c2ca26bd06d844da625b0b
luiz <luizmarcelolm@hotmail.com>
2023-07-01 00:01:44 -0300

    Criando menu principal

---
+++ Calculadora piso/Controllers/PisosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculadora_piso.Controllers
+{
+    public class PisosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Excuir()
+        {
+            return View();
+        }
+    }
+
+   
+}
+
+
